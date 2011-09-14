ac6bf449a1e5e91d3ba7506de8d3af27cb3f9b77
pascal.leclercq <pascal.leclercq@gmail.com>
2011-09-14 15:01:51 +0200

    templateEnginesyCache

--- template/fr.opensagres.xdocreport.template/src/main/java/fr/opensagres/xdocreport/template/registry/TemplateEngineRegistry.java
+++ template/fr.opensagres.xdocreport.template/src/main/java/fr/opensagres/xdocreport/template/registry/TemplateEngineRegistry.java
@@ -1,27 +1,28 @@
 /**
  * Copyright (C) 2011 Angelo Zerr <angelo.zerr@gmail.com> and Pascal Leclercq <pascal.leclercq@gmail.com>
- *
+ * 
  * All rights reserved.
- *
- * Permission is hereby granted, free  of charge, to any person obtaining
- * a  copy  of this  software  and  associated  documentation files  (the
- * "Software"), to  deal in  the Software without  restriction, including
- * without limitation  the rights to  use, copy, modify,  merge, publish,
- * distribute,  sublicense, and/or sell  copies of  the Software,  and to
- * permit persons to whom the Software  is furnished to do so, subject to
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
  * the following conditions:
- *
- * The  above  copyright  notice  and  this permission  notice  shall  be
+ * 
+ * The above copyright notice and this permission notice shall be
  * included in all copies or substantial portions of the Software.
- *
- * THE  SOFTWARE IS  PROVIDED  "AS  IS", WITHOUT  WARRANTY  OF ANY  KIND,
- * EXPRESS OR  IMPLIED, INCLUDING  BUT NOT LIMITED  TO THE  WARRANTIES OF
- * MERCHANTABILITY,    FITNESS    FOR    A   PARTICULAR    PURPOSE    AND
+ * 
+ * THE SOFTWARE IS PROVIDED "AS  IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
  * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
  * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- * OF CONTRACT, TORT OR OTHERWISE,  ARISING FROM, OUT OF OR IN CONNECTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+
 package fr.opensagres.xdocreport.template.registry;
 
 import java.io.Serializable;
@@ -39,105 +40,99 @@ import fr.opensagres.xdocreport.template.discovery.ITemplateEngineDiscovery;
  * template engine kind (Freemarker, Velocity) and document kind (odt, docx..)
  * 
  */
-public class TemplateEngineRegistry extends AbstractRegistry<ITemplateEngineDiscovery> implements
-		Serializable {
+public class TemplateEngineRegistry extends AbstractRegistry<ITemplateEngineDiscovery> implements Serializable
+{
 
-	private static final long serialVersionUID = -7686229087661483932L;
-	private static final String TEMPLATE_DISCOVERY_PROPERTIES = "template-discovery.properties";
-	private static final TemplateEngineRegistry INSTANCE = new TemplateEngineRegistry();
+    private static final long serialVersionUID = -7686229087661483932L;
+    private static final String TEMPLATE_DISCOVERY_PROPERTIES = "template-discovery.properties";
+    private static final TemplateEngineRegistry INSTANCE = new TemplateEngineRegistry();
 
-	//
+    //
 
-	private Map<String, ITemplateEngine> templateEnginesyCache = new HashMap<String, ITemplateEngine>();
-	private Map<String, ITemplateEngineDiscovery> templateEnginesDiscoveryCache = new HashMap<String, ITemplateEngineDiscovery>();
-	private ITemplateEngine defaultTemplateEngine;
-	private Collection<String> templateEngineKinds = new ArrayList<String>();
+    private final Map<String, ITemplateEngine> templateEnginesyCache = new HashMap<String, ITemplateEngine>();
+    private final Map<String, ITemplateEngineDiscovery> templateEnginesDiscoveryCache = new HashMap<String, ITemplateEngineDiscovery>();
+    private ITemplateEngine defaultTemplateEngine;
+    private final Collection<String> templateEngineKinds = new ArrayList<String>();
 
-	public static TemplateEngineRegistry getRegistry() {
-		return INSTANCE;
-	}
+    public static TemplateEngineRegistry getRegistry() {
+        return INSTANCE;
+    }
 
-	public TemplateEngineRegistry() {
-		super(ITemplateEngineDiscovery.class);
-	}
+    public TemplateEngineRegistry() {
+        super(ITemplateEngineDiscovery.class);
+    }
 
-	
-	
-	
-	@Override
-	protected boolean registerInstance(ITemplateEngineDiscovery instance) {
-		
-			ITemplateEngineDiscovery discovery = (ITemplateEngineDiscovery) instance;
-			templateEnginesDiscoveryCache.put(discovery.getId(), discovery);
-			ITemplateEngine templateEngine = discovery.createTemplateEngine();
-			register(templateEngine);
-			return true;
-		
-		
-	}
+    @Override
+    protected boolean registerInstance(ITemplateEngineDiscovery instance) {
 
-	/*@Override
-	protected void initializeIfNeeded() {
-		// TODO Auto-generated method stub
-		super.initializeIfNeeded();
-		
-		Iterator<ITemplateEngineDiscovery> converterDiscoveries = ServiceRegistry.lookupProviders(ITemplateEngineDiscovery.class,getClass().getClassLoader());
-		
-		while (converterDiscoveries.hasNext()) {
-			ITemplateEngineDiscovery instance =  converterDiscoveries.next();
-			
-		boolean result=	registerInstance(instance);
+        ITemplateEngineDiscovery discovery = instance;
+        templateEnginesDiscoveryCache.put(discovery.getId(), discovery);
+        ITemplateEngine templateEngine = discovery.createTemplateEngine();
+        register(templateEngine);
+        return true;
 
-			
-		}
-	}*/
-	private void register(ITemplateEngine templateEngine) {
-		//templateEnginesCache.put(templateEngine.getId(), templateEngine);
-		templateEnginesyCache.put(templateEngine.getKind(), templateEngine);
-		//register(templateEngine, (String) null);
-	}
-	
+    }
 
-	public boolean isDefault(ITemplateEngine templateEngine) {
-		initializeIfNeeded();
-		if (templateEngine == null) {
-			return false;
-		}
-		return templateEngine.equals(defaultTemplateEngine);
-	}
+    /*@Override
+    protected void initializeIfNeeded() {
+    	// TODO Auto-generated method stub
+    	super.initializeIfNeeded();
+    	
+    	Iterator<ITemplateEngineDiscovery> converterDiscoveries = ServiceRegistry.lookupProviders(ITemplateEngineDiscovery.class,getClass().getClassLoader());
+    	
+    	while (converterDiscoveries.hasNext()) {
+    		ITemplateEngineDiscovery instance =  converterDiscoveries.next();
+    		
+    	boolean result=	registerInstance(instance);
 
-	public ITemplateEngine getDefaultTemplateEngine() {
-		initializeIfNeeded();
-		return defaultTemplateEngine;
-	}
+    		
+    	}
+    }*/
+    private void register(ITemplateEngine templateEngine) {
+        // templateEnginesCache.put(templateEngine.getId(), templateEngine);
+        templateEnginesyCache.put(templateEngine.getKind(), templateEngine);
+        // register(templateEngine, (String) null);
+    }
 
-	public void setDefaultTemplateEngine(ITemplateEngine defaultTemplateEngine) {
-		this.defaultTemplateEngine = defaultTemplateEngine;
-	}
+    public boolean isDefault(ITemplateEngine templateEngine) {
+        initializeIfNeeded();
+        if (templateEngine == null) {
+            return false;
+        }
+        return templateEngine.equals(defaultTemplateEngine);
+    }
 
-	
+    public ITemplateEngine getDefaultTemplateEngine() {
+        initializeIfNeeded();
+        return defaultTemplateEngine;
+    }
 
-	public Collection<ITemplateEngine> getTemplateEngines() {
-		initializeIfNeeded();
-		return templateEnginesCache.values();
-	}
+    public void setDefaultTemplateEngine(ITemplateEngine defaultTemplateEngine) {
+        this.defaultTemplateEngine = defaultTemplateEngine;
+    }
 
-	public Collection<String> getTemplateEngineKinds() {
-		initializeIfNeeded();
-		return templateEnginesyCache.keySet();
-		//return templateEngineKinds;
-	}
+    public Collection<ITemplateEngine> getTemplateEngines() {
+        initializeIfNeeded();
+        return templateEnginesyCache.values();
+    }
 
-	public Map<String, ITemplateEngineDiscovery> getTemplateEnginesDiscoveryCache() {
-		return templateEnginesDiscoveryCache;
-	}
-	@Override
-	protected void doDispose() {
-	//	this.templateEnginesCache.clear();
-		//this.templateEnginesInitializerDiscoveryCache.clear();
-		this.templateEnginesDiscoveryCache.clear();
-		this.defaultTemplateEngine = null;
-		this.templateEngineKinds.clear();
+    public Collection<String> getTemplateEngineKinds() {
+        initializeIfNeeded();
+        return templateEnginesyCache.keySet();
+        // return templateEngineKinds;
+    }
 
-	}
+    public Map<String, ITemplateEngineDiscovery> getTemplateEnginesDiscoveryCache() {
+        return templateEnginesDiscoveryCache;
+    }
+
+    @Override
+    protected void doDispose() {
+        // this.templateEnginesCache.clear();
+        // this.templateEnginesInitializerDiscoveryCache.clear();
+        this.templateEnginesDiscoveryCache.clear();
+        this.defaultTemplateEngine = null;
+        this.templateEngineKinds.clear();
+
+    }
 }

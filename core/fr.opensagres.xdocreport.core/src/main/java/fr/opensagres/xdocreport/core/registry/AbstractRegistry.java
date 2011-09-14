cbe955a90d205bb9054dc76144be2c9cbd400f47
pascal.leclercq <pascal.leclercq@gmail.com>
2011-09-14 15:26:39 +0200

    initialized = true; à la fin de initialized if needed

--- core/fr.opensagres.xdocreport.core/src/main/java/fr/opensagres/xdocreport/core/registry/AbstractRegistry.java
+++ core/fr.opensagres.xdocreport.core/src/main/java/fr/opensagres/xdocreport/core/registry/AbstractRegistry.java
@@ -1,27 +1,28 @@
 /**
  * Copyright (C) 2011 Angelo Zerr <angelo.zerr@gmail.com> and Pascal Leclercq <pascal.leclercq@gmail.com>
- *
+ * 
  * All rights reserved.
- *
- * Permission is hereby granted, free  of charge, to any person obtaining
- * a  copy  of this  software  and  associated  documentation files  (the
- * "Software"), to  deal in  the Software without  restriction, including
- * without limitation  the rights to  use, copy, modify,  merge, publish,
- * distribute,  sublicense, and/or sell  copies of  the Software,  and to
- * permit persons to whom the Software  is furnished to do so, subject to
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
  * the following conditions:
- *
- * The  above  copyright  notice  and  this permission  notice  shall  be
+ * 
+ * The above copyright notice and this permission notice shall be
  * included in all copies or substantial portions of the Software.
- *
- * THE  SOFTWARE IS  PROVIDED  "AS  IS", WITHOUT  WARRANTY  OF ANY  KIND,
- * EXPRESS OR  IMPLIED, INCLUDING  BUT NOT LIMITED  TO THE  WARRANTIES OF
- * MERCHANTABILITY,    FITNESS    FOR    A   PARTICULAR    PURPOSE    AND
+ * 
+ * THE SOFTWARE IS PROVIDED "AS  IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
  * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
  * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- * OF CONTRACT, TORT OR OTHERWISE,  ARISING FROM, OUT OF OR IN CONNECTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+
 package fr.opensagres.xdocreport.core.registry;
 
 import java.util.Iterator;
@@ -33,74 +34,77 @@ import javax.imageio.spi.ServiceRegistry;
 import fr.opensagres.xdocreport.core.discovery.IBaseDiscovery;
 import fr.opensagres.xdocreport.core.logging.LogUtils;
 
-public abstract class AbstractRegistry<Discovery extends IBaseDiscovery> {
-	/**
-	 * Logger for this class
-	 */
-	private static final Logger LOGGER = LogUtils.getLogger(AbstractRegistry.class.getName());
+public abstract class AbstractRegistry<Discovery extends IBaseDiscovery>
+{
 
-	private boolean initialized;
+    /**
+     * Logger for this class
+     */
+    private static final Logger LOGGER = LogUtils.getLogger(AbstractRegistry.class.getName());
 
-	private final Class<Discovery> registryType;
+    private boolean initialized;
 
-	public AbstractRegistry(Class<Discovery> registryType) {
-		this.registryType = registryType;
-	}
+    private final Class<Discovery> registryType;
 
-	public void initialize() {
-		initializeIfNeeded();
-	}
+    public AbstractRegistry(Class<Discovery> registryType) {
+        this.registryType = registryType;
+    }
 
-	protected  void initializeIfNeeded() {
-		if (!initialized) {
-			onStartInitialization();
-			//getClass().getClassLoader() to work under OSGi context
-			Iterator<Discovery> discoveries = ServiceRegistry.lookupProviders(registryType, getClass().getClassLoader());
-			if(LOGGER.isLoggable(Level.FINE)){
-				LOGGER.fine("discoveries found ? "+ discoveries.hasNext());	
-			}
-			
-			while (discoveries.hasNext()) {
-				Discovery instance = discoveries.next();
-				boolean result = registerInstance(instance);
-				if(LOGGER.isLoggable(Level.FINE)){
-					LOGGER.fine("Registered Discovery instance  "+ instance+ " "+ result);
-				}
-			}
-			onEndInitialization();
-		}
-	}
+    public void initialize() {
+        initializeIfNeeded();
+    }
 
-	/**
-	 * Method called when registry start initialization.
-	 */
-	protected void onStartInitialization() {
+    protected void initializeIfNeeded() {
+        if (!initialized) {
+            onStartInitialization();
+            // getClass().getClassLoader() to work under OSGi context
+            Iterator<Discovery> discoveries = ServiceRegistry
+                    .lookupProviders(registryType, getClass().getClassLoader());
+            if (LOGGER.isLoggable(Level.FINE)) {
+                LOGGER.fine("discoveries found ? " + discoveries.hasNext());
+            }
 
-	}
+            while (discoveries.hasNext()) {
+                Discovery instance = discoveries.next();
+                boolean result = registerInstance(instance);
+                if (LOGGER.isLoggable(Level.FINE)) {
+                    LOGGER.fine("Registered Discovery instance  " + instance + " " + result);
+                }
+            }
+            onEndInitialization();
+            initialized = true;
+        }
+    }
 
-	/**
-	 * Method called when registry end initialization.
-	 */
-	protected void onEndInitialization() {
+    /**
+     * Method called when registry start initialization.
+     */
+    protected void onStartInitialization() {
 
-	}
+    }
 
-	/**
-	 * Register the instance in the registry.
-	 * 
-	 * @param instance
-	 * @return true if instance cannot be registered and false otherwise.
-	 */
-	protected abstract boolean registerInstance(Discovery instance);
+    /**
+     * Method called when registry end initialization.
+     */
+    protected void onEndInitialization() {
 
+    }
 
-	/**
-	 * Dispose the registry.
-	 */
-	public final void dispose() {
-		doDispose();
-		this.initialized = false;
-	}
+    /**
+     * Register the instance in the registry.
+     * 
+     * @param instance
+     * @return true if instance cannot be registered and false otherwise.
+     */
+    protected abstract boolean registerInstance(Discovery instance);
 
-	protected abstract void doDispose();
+    /**
+     * Dispose the registry.
+     */
+    public final void dispose() {
+        doDispose();
+        this.initialized = false;
+    }
+
+    protected abstract void doDispose();
 }

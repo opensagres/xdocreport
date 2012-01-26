5e6598846ecb88604b835d91b4ff36d11e604667
abezif <abezif@gmail.com>
2012-01-26 09:57:34 +0100

    odf->pdf section conversion - processing styles

---
+++ thirdparties-extension/org.odftoolkit.odfdom.converter/src/main/java/org/odftoolkit/odfdom/converter/internal/itext/styles/StyleColumnsProperties.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (C) 2011 Angelo Zerr <angelo.zerr@gmail.com> and Pascal Leclercq <pascal.leclercq@gmail.com>
+ *
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free  of charge, to any person obtaining
+ * a  copy  of this  software  and  associated  documentation files  (the
+ * "Software"), to  deal in  the Software without  restriction, including
+ * without limitation  the rights to  use, copy, modify,  merge, publish,
+ * distribute,  sublicense, and/or sell  copies of  the Software,  and to
+ * permit persons to whom the Software  is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The  above  copyright  notice  and  this permission  notice  shall  be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE  SOFTWARE IS  PROVIDED  "AS  IS", WITHOUT  WARRANTY  OF ANY  KIND,
+ * EXPRESS OR  IMPLIED, INCLUDING  BUT NOT LIMITED  TO THE  WARRANTIES OF
+ * MERCHANTABILITY,    FITNESS    FOR    A   PARTICULAR    PURPOSE    AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE,  ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.odftoolkit.odfdom.converter.internal.itext.styles;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * fixes for pdf conversion by Leszek Piotrowicz <leszekp@safe-mail.net>
+ */
+public class StyleColumnsProperties {
+
+	// attributes
+	private Integer columnCount;
+	// child nodes
+	private List<StyleColumnProperties> columnProperties = new ArrayList<StyleColumnProperties>();
+
+	public StyleColumnsProperties() {
+	}
+
+	public Integer getColumnCount() {
+		return columnCount;
+	}
+
+	public void setColumnCount(Integer columnCount) {
+		this.columnCount = columnCount;
+	}
+
+	public List<StyleColumnProperties> getColumnProperties() {
+		return columnProperties;
+	}
+}

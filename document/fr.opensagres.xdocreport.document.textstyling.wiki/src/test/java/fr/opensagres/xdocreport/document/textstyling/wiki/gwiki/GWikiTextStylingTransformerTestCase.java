984a3e973f2e2e1315a115b6bfb2ee3236757eb0
angelo.zerr@gmail.com <angelo.zerr@gmail.com>
2012-01-26 09:51:37 +0100

    Fix pb with JUNit.

--- document/fr.opensagres.xdocreport.document.textstyling.wiki/src/test/java/fr/opensagres/xdocreport/document/textstyling/wiki/gwiki/GWikiTextStylingTransformerTestCase.java
+++ document/fr.opensagres.xdocreport.document.textstyling.wiki/src/test/java/fr/opensagres/xdocreport/document/textstyling/wiki/gwiki/GWikiTextStylingTransformerTestCase.java
@@ -26,6 +26,6 @@ public class GWikiTextStylingTransformerTestCase {
 +"\n # _*BoldAndItalic*_ style.", handler);
 		Assert.assertEquals(
 				"<p>Here are severals styles</p><ul><li><strong>Bold</strong> style</li><li><i>Italic</i> style</li><li><i><strong>BoldAndItalic</i></strong> style</li></ul>Here are 3 styles<ol><li><strong>Bold</strong> style</li><li><i>Italic</i> style</li><li><i><strong>BoldAndItalic</i></strong> style</li></ol>",
-				handler.toString());
+				handler.getTextBody());
 	}
 }

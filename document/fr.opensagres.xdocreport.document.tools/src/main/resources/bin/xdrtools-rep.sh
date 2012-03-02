9ea7be0542acc3e21f3dc2f65c58ae1babd45938
Thierry Delprat <tdelprat@nuxeo.com>
2012-03-02 14:29:45 +0100

    Add sh file to run xdrtools on non Vindoz boxes

---
+++ document/fr.opensagres.xdocreport.document.tools/src/main/resources/bin/xdrtools-rep.sh
@@ -0,0 +1,65 @@
+#! /bin/sh
+BASE_ADDRESS=$1
+if [ -n "$BASE_ADDRESS" ]
+then
+   error
+fi
+
+USER=$2
+if [ -n "$USER" ]
+then
+   error
+fi
+
+PASSWORD=$3
+if [ -n "$PASSWORD" ]
+then
+   error
+fi
+
+SERVICE_TYPE=$4
+if [ -n "$SERVICE_TYPE" ]
+then
+   error
+fi
+
+SERVICE_NAME=$5
+if [ -n "$SERVICE_NAME" ]
+then
+   error
+fi
+
+OUT=$6
+if [ -n "$OUT" ]
+then
+   error
+fi
+
+ERR=$7
+if [ -n "$ERR" ]
+then
+   error
+fi
+
+RESOURCES=$8
+if [ -n "$RESOURCES" ]
+then
+   error
+fi
+
+
+cd `dirname $0`
+BASE_DIR=`pwd`
+
+for i in `ls  $BASE_DIR/../lib/*.jar`
+    do
+        CLASSPATH=$CLASSPATH:$i
+    done
+
+java -classpath "$CLASSPATH" fr.opensagres.xdocreport.document.tools.remoting.resources.Main -baseAddress $BASE_ADDRESS -user $USER -password $PASSWORD -serviceType $SERVICE_TYPE -serviceName $SERVICE_NAME -out $OUT -err $ERR -resources $RESOURCES
+
+
+error()
+{
+  echo  "Impossible to launch XDocReport-Repository Tools."
+}

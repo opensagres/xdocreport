a9b2191b30d13b39125f21890bbaea8785aaef7e
angelo.zerr@gmail.com <angelo.zerr@gmail.com>
2012-03-08 18:18:42 +0100

    Create java command parameters with OOo extension.

--- document/fr.opensagres.xdocreport.document.tools/src/main/resources/bin/xdrtools-rep.bat
+++ document/fr.opensagres.xdocreport.document.tools/src/main/resources/bin/xdrtools-rep.bat
@@ -25,14 +25,7 @@
 
 @echo on
 
-set BASE_ADDRESS=%1
-set USER=%2
-set PASSWORD=%3
-set SERVICE_TYPE=%4
-set SERVICE_NAME=%5
-set OUT=%6
-set ERR=%7
-set RESOURCES=%8
+set PARAM=%1
 
 setlocal EnableDelayedExpansion
 set CLASSPATH="
@@ -41,7 +34,7 @@ for /R %~dp0/../lib %%a in (*.jar) do (
 )
 set CLASSPATH=!CLASSPATH!"
 
-java -classpath "%CLASSPATH%" fr.opensagres.xdocreport.document.tools.remoting.resources.Main -baseAddress %BASE_ADDRESS% -user %USER% -password %PASSWORD% -serviceType %SERVICE_TYPE% -serviceName %SERVICE_NAME% -out %OUT% -err %ERR% -resources %RESOURCES%
+java -classpath "%CLASSPATH%" fr.opensagres.xdocreport.document.tools.remoting.resources.Main %PARAM%
 
 goto end
 
--- document/fr.opensagres.xdocreport.document.tools/src/main/resources/bin/xdrtools-rep.sh
+++ document/fr.opensagres.xdocreport.document.tools/src/main/resources/bin/xdrtools-rep.sh
@@ -1,52 +1,9 @@
 #! /bin/sh
-BASE_ADDRESS=$1
-if [ -n "$BASE_ADDRESS" ]
+PARAM=$1
+if [ -n "$PARAM" ]
 then
    error
 fi
-
-USER=$2
-if [ -n "$USER" ]
-then
-   error
-fi
-
-PASSWORD=$3
-if [ -n "$PASSWORD" ]
-then
-   error
-fi
-
-SERVICE_TYPE=$4
-if [ -n "$SERVICE_TYPE" ]
-then
-   error
-fi
-
-SERVICE_NAME=$5
-if [ -n "$SERVICE_NAME" ]
-then
-   error
-fi
-
-OUT=$6
-if [ -n "$OUT" ]
-then
-   error
-fi
-
-ERR=$7
-if [ -n "$ERR" ]
-then
-   error
-fi
-
-RESOURCES=$8
-if [ -n "$RESOURCES" ]
-then
-   error
-fi
-
 
 cd `dirname $0`
 BASE_DIR=`pwd`
@@ -56,8 +13,7 @@ for i in `ls  $BASE_DIR/../lib/*.jar`
         CLASSPATH=$CLASSPATH:$i
     done
 
-java -classpath "$CLASSPATH" fr.opensagres.xdocreport.document.tools.remoting.resources.Main -baseAddress $BASE_ADDRESS -user $USER -password $PASSWORD -serviceType $SERVICE_TYPE -serviceName $SERVICE_NAME -out $OUT -err $ERR -resources $RESOURCES
-
+java -classpath "$CLASSPATH" fr.opensagres.xdocreport.document.tools.remoting.resources.Main $PARAM
 
 error()
 {

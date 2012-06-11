684203736ed10e629ba15c3f092e8c9e12dea218
pascal leclercq <pascal.leclercq@gmail.com>
2012-06-11 23:21:33 +0200

    NEW - task 118: Improve Remoting API http://code.google.com/p/xdocreport/issues/detail?id=118
    Contract first JAXWS

--- remoting/fr.opensagres.xdocreport.remoting.resources/src/main/java/fr/opensagres/xdocreport/remoting/resources/domain/DownloadMultiple.java
+++ remoting/fr.opensagres.xdocreport.remoting.resources/src/main/java/fr/opensagres/xdocreport/remoting/resources/domain/DownloadMultiple.java
@@ -5,21 +5,20 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
 
 
 /**
- * <p>Classe Java pour filter complex type.
+ * <p>Classe Java pour downloadMultiple complex type.
  * 
  * <p>Le fragment de schéma suivant indique le contenu attendu figurant dans cette classe.
  * 
  * <pre>
- * &lt;complexType name="filter">
+ * &lt;complexType name="downloadMultiple">
  *   &lt;complexContent>
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;sequence>
- *         &lt;element name="properties" type="{http://domain.resources.remoting.xdocreport.opensagres.fr/}propertyRepresentation" maxOccurs="unbounded" minOccurs="0"/>
+ *         &lt;element name="resourceIds" type="{http://www.w3.org/2001/XMLSchema}string" maxOccurs="unbounded" minOccurs="0"/>
  *       &lt;/sequence>
  *     &lt;/restriction>
  *   &lt;/complexContent>
@@ -29,41 +28,40 @@ import javax.xml.bind.annotation.XmlType;
  * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "filter", propOrder = {
-    "properties"
+@XmlType(name = "downloadMultiple", propOrder = {
+    "resourceIds"
 })
-public class Filter {
+public class DownloadMultiple {
 
-    @XmlElement(nillable = true)
-    protected List<PropertyRepresentation> properties;
+    protected List<String> resourceIds;
 
     /**
-     * Gets the value of the properties property.
+     * Gets the value of the resourceIds property.
      * 
      * <p>
      * This accessor method returns a reference to the live list,
      * not a snapshot. Therefore any modification you make to the
      * returned list will be present inside the JAXB object.
-     * This is why there is not a <CODE>set</CODE> method for the properties property.
+     * This is why there is not a <CODE>set</CODE> method for the resourceIds property.
      * 
      * <p>
      * For example, to add a new item, do as follows:
      * <pre>
-     *    getProperties().add(newItem);
+     *    getResourceIds().add(newItem);
      * </pre>
      * 
      * 
      * <p>
      * Objects of the following type(s) are allowed in the list
-     * {@link PropertyRepresentation }
+     * {@link String }
      * 
      * 
      */
-    public List<PropertyRepresentation> getProperties() {
-        if (properties == null) {
-            properties = new ArrayList<PropertyRepresentation>();
+    public List<String> getResourceIds() {
+        if (resourceIds == null) {
+            resourceIds = new ArrayList<String>();
         }
-        return this.properties;
+        return this.resourceIds;
     }
 
 }
--- remoting/fr.opensagres.xdocreport.remoting.resources/src/main/java/fr/opensagres/xdocreport/remoting/resources/domain/DownloadMultipleResponse.java
+++ remoting/fr.opensagres.xdocreport.remoting.resources/src/main/java/fr/opensagres/xdocreport/remoting/resources/domain/DownloadMultipleResponse.java
@@ -10,16 +10,16 @@ import javax.xml.bind.annotation.XmlType;
 
 
 /**
- * <p>Classe Java pour filter complex type.
+ * <p>Classe Java pour downloadMultipleResponse complex type.
  * 
  * <p>Le fragment de schéma suivant indique le contenu attendu figurant dans cette classe.
  * 
  * <pre>
- * &lt;complexType name="filter">
+ * &lt;complexType name="downloadMultipleResponse">
  *   &lt;complexContent>
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;sequence>
- *         &lt;element name="properties" type="{http://domain.resources.remoting.xdocreport.opensagres.fr/}propertyRepresentation" maxOccurs="unbounded" minOccurs="0"/>
+ *         &lt;element name="return" type="{http://domain.resources.remoting.xdocreport.opensagres.fr/}binaryData" maxOccurs="unbounded" minOccurs="0"/>
  *       &lt;/sequence>
  *     &lt;/restriction>
  *   &lt;/complexContent>
@@ -29,41 +29,41 @@ import javax.xml.bind.annotation.XmlType;
  * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "filter", propOrder = {
-    "properties"
+@XmlType(name = "downloadMultipleResponse", propOrder = {
+    "_return"
 })
-public class Filter {
+public class DownloadMultipleResponse {
 
-    @XmlElement(nillable = true)
-    protected List<PropertyRepresentation> properties;
+    @XmlElement(name = "return")
+    protected List<BinaryData> _return;
 
     /**
-     * Gets the value of the properties property.
+     * Gets the value of the return property.
      * 
      * <p>
      * This accessor method returns a reference to the live list,
      * not a snapshot. Therefore any modification you make to the
      * returned list will be present inside the JAXB object.
-     * This is why there is not a <CODE>set</CODE> method for the properties property.
+     * This is why there is not a <CODE>set</CODE> method for the return property.
      * 
      * <p>
      * For example, to add a new item, do as follows:
      * <pre>
-     *    getProperties().add(newItem);
+     *    getReturn().add(newItem);
      * </pre>
      * 
      * 
      * <p>
      * Objects of the following type(s) are allowed in the list
-     * {@link PropertyRepresentation }
+     * {@link BinaryData }
      * 
      * 
      */
-    public List<PropertyRepresentation> getProperties() {
-        if (properties == null) {
-            properties = new ArrayList<PropertyRepresentation>();
+    public List<BinaryData> getReturn() {
+        if (_return == null) {
+            _return = new ArrayList<BinaryData>();
         }
-        return this.properties;
+        return this._return;
     }
 
 }

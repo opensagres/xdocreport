016fe2069db0dfaf29426187d340487339a9e4c8
pascal leclercq <pascal.leclercq@gmail.com>
2012-10-06 23:09:31 +0200

    STARTED - task 152: Develop REST Service Converter + WebApplication http://code.google.com/p/xdocreport/issues/detail?id=152 cleanup

--- remoting/fr.opensagres.xdocreport.remoting.converter/src/main/java/fr/opensagres/xdocreport/converter/ConverterResource.java
+++ remoting/fr.opensagres.xdocreport.remoting.converter/src/main/java/fr/opensagres/xdocreport/converter/ConverterResource.java
@@ -2,7 +2,6 @@ package fr.opensagres.xdocreport.converter;
 
 import javax.activation.DataSource;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -14,10 +13,6 @@ import org.apache.cxf.jaxrs.ext.multipart.Multipart;
 public interface ConverterResource
 {
 
-    @GET
-    @Path( "hello" )
-    @Produces( "text/plain" )
-    String getText();
 
     @POST
     @Path( "convertPDF" )
@@ -25,14 +20,6 @@ public interface ConverterResource
     @Produces( MediaType.WILDCARD )
     BinaryFile convertPDF( Request request );
 
-    @POST
-    @Consumes( MediaType.WILDCARD )
-    @Produces( MediaType.WILDCARD )
-    @Path( "/submitForm" )
-    BinaryFile submitForm( @Multipart( "outputFormat" )
-    String outputFormat, @Multipart( "datafile" )
-    DataSource content, @Multipart( "operation" )
-    String operation );
 
     @POST
     @Consumes( MediaType.WILDCARD )

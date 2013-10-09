1cc325d6479d4ebdc930ea2cdfad8212a9d3e885
pascal leclercq <pascal.leclercq@gmail.com>
2013-10-10 00:28:56 +0200

    https://code.google.com/p/xdocreport/issues/detail?id=314
    small refactor

--- document/fr.opensagres.xdocreport.document/src/main/java/fr/opensagres/xdocreport/document/dump/DumperWithFreemarker.java
+++ document/fr.opensagres.xdocreport.document/src/main/java/fr/opensagres/xdocreport/document/dump/DumperWithFreemarker.java
@@ -6,10 +6,6 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.StringWriter;
 import java.io.Writer;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import fr.opensagres.xdocreport.core.XDocReportException;
 import fr.opensagres.xdocreport.core.io.IOUtils;
@@ -18,10 +14,8 @@ import fr.opensagres.xdocreport.core.utils.Base64Utility;
 import fr.opensagres.xdocreport.core.utils.StringUtils;
 import fr.opensagres.xdocreport.document.IXDocReport;
 import fr.opensagres.xdocreport.document.json.JSONObject;
-import fr.opensagres.xdocreport.document.registry.XDocReportRegistry;
 import fr.opensagres.xdocreport.template.IContext;
 import fr.opensagres.xdocreport.template.ITemplateEngine;
-import fr.opensagres.xdocreport.template.TemplateEngineKind;
 import fr.opensagres.xdocreport.template.formatter.FieldsMetadata;
 import fr.opensagres.xdocreport.template.registry.TemplateEngineRegistry;
 
@@ -31,60 +25,7 @@ public class DumperWithFreemarker
     private static final String ESCAPED_DOUBLE_QUOTE = "\\\"";
 	private static final String DOUBLE_QUOTE = "\"";
 
-	public static void main( String[] args )
-    {
-        try
-        {
-            // 1) Load Docx file by filling Velocity template engine and cache
-            // it to the registry
-            String reportId = "DumTest";
-            InputStream in = DumperWithFreemarker.class.getResourceAsStream( "DocxProjectWithFreemarkerList.docx" );
-            IXDocReport report =
-                XDocReportRegistry.getRegistry().loadReport( in, reportId, TemplateEngineKind.Freemarker );
-
-            // IMPORTANT : cache the original document to use dump.
-            report.setCacheOriginalDocument( true );
-
-            FieldsMetadata metadata = report.createFieldsMetadata();
-            metadata.addFieldAsList( "developers.name" );
-            metadata.addFieldAsList( "developers.lastName" );
-            metadata.addFieldAsList( "developers.mail" );
-
-            // 2) Create context Java model
-            IContext context = report.createContext();
-            context.put( "name", "XDocReport" );
-            List<Map<String, String>> developers = new ArrayList<Map<String, String>>();
-            Map<String, String> developer1 = new HashMap<String, String>();
-            developer1.put( "Name", "ZERR" );
-            developer1.put( "LastName", "Angelo" );
-            developer1.put( "Mail", "angelo.zerr@gmail.com" );
-            developers.add( developer1 );
-            Map<String, String> developer2 = new HashMap<String, String>();
-            developer2.put( "Name", "Leclercq" );
-            developer2.put( "LastName", "Pascal" );
-            developer2.put( "Mail", "pascal.leclercq@gmail.com" );
-            developers.add( developer2 );
-            context.put( "developers", developers );
-
-            Writer writer = new StringWriter();
-
-            dump( report, context, writer );
-
-            System.err.println( writer );
-
-        }
-        catch ( IOException e )
-        {
-            e.printStackTrace();
-        }
-        catch ( XDocReportException e )
-        {
-            e.printStackTrace();
-        }
-
-    }
-
-    private static void dump( IXDocReport report, IContext context, Writer writer )
+    public static void dump( IXDocReport report, IContext context, Writer writer )
         throws XDocReportException, IOException
     {
 
@@ -121,6 +62,7 @@ public class DumperWithFreemarker
 
         Reader reader = new InputStreamReader( DumperWithFreemarker.class.getResourceAsStream( "JavaMainDump.ftl" ) );
         templateEngine.process( "dump", dumpContext, reader, writer );
+        
     }
 
     private static String getDocument( IXDocReport report )

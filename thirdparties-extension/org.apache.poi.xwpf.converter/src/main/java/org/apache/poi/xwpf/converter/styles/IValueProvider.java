6e1ddceaf1524a7ef8901d6205e18c29528d26cb
pascalleclercq <pascal.leclercq@gmail.com>
2014-06-10 23:05:30 +0200

    cleanup org.apache.poi.xwpf.converter

--- thirdparties-extension/org.apache.poi.xwpf.converter/src/main/java/org/apache/poi/xwpf/converter/styles/IValueProvider.java
+++
@@ -1,7 +0,0 @@
-package org.apache.poi.xwpf.converter.styles;
-
-public interface IValueProvider<Value, XWPElement>
-{
-    Value getValue( XWPElement element, XWPFStylesDocument styleManager );
-
-}

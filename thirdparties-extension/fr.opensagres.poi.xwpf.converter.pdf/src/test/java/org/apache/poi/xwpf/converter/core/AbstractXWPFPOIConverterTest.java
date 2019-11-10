d412e2b0d850ab16cdd573aad0b4ece248d5407f
sammad <sambharmadhur@gmail.com>
2019-11-10 11:33:06 +0530

    Revert "Issue#390: When the docx header has a hyperlink, email address or image it was adding a null anchor .e. #null which was creating an unused annotation. Added change not to add the anchor in case anchor is null. "
    This reverts commit 1165cdfe05fa2f7950cb8f30f407180d547acf85.

--- thirdparties-extension/fr.opensagres.poi.xwpf.converter.pdf/src/test/java/org/apache/poi/xwpf/converter/core/AbstractXWPFPOIConverterTest.java
+++ thirdparties-extension/fr.opensagres.poi.xwpf.converter.pdf/src/test/java/org/apache/poi/xwpf/converter/core/AbstractXWPFPOIConverterTest.java
@@ -528,20 +528,7 @@ public abstract class AbstractXWPFPOIConverterTest
     {
         doGenerate( "TestSuperscriptSubscript.docx" );
     }
-    
-    @Test
-    public void TestHeaderWithHyperlink()
-            throws IOException
-    {
-        doGenerate( "TestHeaderWithHyperlink.docx" );
-    }
-    
-    @Test
-    public void TestHeaderWithImage()
-            throws IOException
-    {
-        doGenerate( "TestHeaderWithImage.docx" );
-    }
+
     protected abstract void doGenerate( String fileName )
         throws IOException;
 
